efa8d38c2454b5f199a2876ba89238b2231924fb
grantcolley <grantcolley@yahoo.com>
2018-10-18 21:07:40 +0100

    New DipSocket proj
    New DipSocket proj

---
+++ src/DipSocket/WebSocketConnections.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DipSocket
+{
+    public class WebSocketConnections
+    {
+        private ConcurrentDictionary<string, WebSocket> webSockets;
+
+        public WebSocketConnections()
+        {
+            webSockets = new ConcurrentDictionary<string, WebSocket>();
+        }
+
+        internal WebSocket GetWebSocket(string connectionId)
+        {
+            if(string.IsNullOrWhiteSpace(connectionId))
+            {
+                return null;
+            }
+
+            return webSockets.FirstOrDefault(ws => ws.Key.Equals(connectionId)).Value;
+        }
+
+        internal ConcurrentDictionary<string, WebSocket> GetWebSockets()
+        {
+            return webSockets;
+        }
+
+        internal string GetConnectionId(WebSocket webSocket)
+        {
+            return webSockets.FirstOrDefault(ws => ws.Value.Equals(webSocket)).Key;
+        }
+
+        internal bool TryAddWebSocket(WebSocket webSocket)
+        {
+            var connectionId = NewConnectionId();
+            return webSockets.TryAdd(connectionId, webSocket);
+        }
+
+        internal async Task TryRemoveWebSocket(WebSocket webSocket)
+        {
+            var connectionId = GetConnectionId(webSocket);
+
+            WebSocket socket;
+            if (webSockets.TryRemove(connectionId, out socket))
+            {
+                await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closed by the WebSocketController", CancellationToken.None);
+            }
+        }
+
+        private string NewConnectionId()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ src/DipSocket/WebSocketServer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("DipSocket.NetCore.Extensions")]
+namespace DipSocket
+{
+    public abstract class WebSocketServer
+    {
+        private WebSocketConnections webSocketConnections;
+
+        public abstract Task ReceiveAsync(WebSocket webSocket, WebSocketReceiveResult webSocketReceiveResult, byte[] buffer);
+
+        public virtual async Task<bool> OnConnectAsync(WebSocket websocket)
+        {
+            return webSocketConnections.TryAddWebSocket(websocket);
+        }
+
+        public virtual Task OnDisonnectAsync(WebSocket webSocket)
+        {
+            return webSocketConnections.TryRemoveWebSocket(webSocket);
+        }
+
+        public async Task SendMessageAsync(WebSocket webSocket, string message)
+        {
+            if(!webSocket.State.Equals(WebSocketState.Open))
+            {
+                return;
+            }
+
+            await webSocket.SendAsync(
+                new ArraySegment<byte>(Encoding.ASCII.GetBytes(message), 0, message.Length), 
+                WebSocketMessageType.Text, true, CancellationToken.None)
+                .ConfigureAwait(false); 
+        }
+
+        public async Task SendMessageAsync(string connectionId, string message)
+        {
+            var webSocket = webSocketConnections.GetWebSocket(connectionId);
+            await SendMessageAsync(webSocket, message).ConfigureAwait(false);
+        }
+
+        public async Task SendMessageToAll(string message)
+        {
+            foreach(var webSocket in webSocketConnections.GetWebSockets())
+            {
+                await SendMessageAsync(webSocket.Value, message).ConfigureAwait(false);
+            }
+        }
+
+        internal void AddWebSocketConnections(WebSocketConnections webSocketConnections)
+        {
+            this.webSocketConnections = webSocketConnections;
+        }
+    }
+}
