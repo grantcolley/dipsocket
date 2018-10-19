c74e76a44f5b23c2c1da3e408bb06b154b32bc0e
grantcolley <grantcolley@yahoo.com>
2018-10-19 20:49:04 +0100

    Skeleton for WebSocketConnection
    Skeleton for WebSocketConnection

---
+++ src/DipSocket/WebSocketConnection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DipSocket
+{
+    public class WebSocketConnection
+    {
+        public event Func<Exception, Task> Closed;
+
+        public Task DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDisposable On(string methodName, Action<object> handler)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnClose(Exception exception)
+        {
+            var closed = Closed;
+            closed.Invoke(exception);
+        }
+    }
+}
