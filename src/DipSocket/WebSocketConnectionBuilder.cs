09aa54686cbcb684ca8bd23928da8cd4bc298ea8
grantcolley <grantcolley@yahoo.com>
2018-10-20 10:18:56 +0100

    Add skeleton for WebSocketConnectionBuilder
    Add skeleton for WebSocketConnectionBuilder

---
+++ src/DipSocket/WebSocketConnectionBuilder.cs
@@ -0,0 +1,22 @@
+﻿namespace DipSocket
+{
+    public class WebSocketConnectionBuilder
+    {
+        private WebSocketConnection WebSocketConnection;
+
+        private WebSocketConnectionBuilder()
+        {
+            WebSocketConnection = new WebSocketConnection();
+        }
+
+        public WebSocketConnection WithUrl(string url)
+        {
+            return WebSocketConnection;
+        }
+
+        public WebSocketConnection Build()
+        {
+            return WebSocketConnection;
+        }
+    }
+}
