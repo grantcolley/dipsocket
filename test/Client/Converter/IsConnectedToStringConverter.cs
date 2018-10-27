d13cf5ba03b36e00f843fa5b49f98ccc2950288f
grantcolley <grantcolley@yahoo.com>
2018-10-27 19:27:55 +0100

    dev in progress...
    dev in progress...

---
+++ test/Client/Converter/IsConnectedToStringConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Client.Converter
+{
+    public sealed class IsConnectedToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null
+                || !(value is bool))
+            {
+                return null;
+            }
+
+            try
+            {
+                if ((bool)value == true)
+                {
+                    return "Disconnect";
+                }
+
+                return "Connect";
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
