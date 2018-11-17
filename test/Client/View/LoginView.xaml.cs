3d198bb1b4b02f212019fcaad3f5a7153257c44c
grantcolley <grantcolley@yahoo.com>
2018-11-17 21:23:07 +0000

    Add LoginView and remove obsolete converters
    Add LoginView and remove obsolete converters

---
+++ test/Client/View/LoginView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace Client.View
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        public string UserName { get { return userName.Text; } }
+
+        private void btnOkClick(object sender, RoutedEventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(userName.Text))
+            {
+                return;
+            }
+
+            DialogResult = true;
+            Close();
+        }
+    }
+}
