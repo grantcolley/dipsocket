58357f1d9a7a28faa46743b86c043da2c9941e28
grantcolley <grantcolley@yahoo.com>
2018-12-31 17:08:34 +0000

    cleanup
    cleanup

--- test/Server/Startup.cs
+++ test/Server/Startup.cs
@@ -22,7 +22,7 @@ namespace Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDipSocket();
+            services.AddDipSocket<Chat>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
